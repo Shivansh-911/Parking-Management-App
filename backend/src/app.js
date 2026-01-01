143b34782fb617aa88956f4bf8857d826f3ccabd
Shivansh-911 <shivanshporwal2003@gmail.com>
2026-01-02 03:38:38 +0530

    fourth commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: '*',
+    origin: '0.0.0.0',
     credentials: true
 }))
 app.use(express.json({limit:'16kb'}))

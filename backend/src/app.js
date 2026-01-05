503dd5003e634a54ae00bf9b1f18cd8663929eec
Shivansh-911 <shivanshporwal2003@gmail.com>
2026-01-06 00:56:26 +0530

    commented the CORS middleware in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,10 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }))
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded())
 app.use(express.static("public"))
